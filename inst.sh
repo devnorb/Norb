3b0d2bdb20146325fc2cfcaf8dd89bb6c23646bb
norby.dev <127006485+devnorb@users.noreply.github.com>
2024-10-07 10:23:56 -0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -14,13 +14,17 @@ main() {
     xattr -c /Applications/Roblox.app
     echo -e "[...] Downloading main executable"
     curl "https://raw.githubusercontent.com/devnorb/Norb/refs/heads/main/macsploit.zip" -o "./macsploit.zip"
+    curl "https://raw.githubusercontent.com/devnorb/Norb/refs/heads/main/libdiscord-rpc.zip" -o "./discord.zip"
     unzip -o -q "./macsploit.zip"
+    unzip -o -q "./discord.zip"
     mv ./macsploit.dylib /Applications/Roblox.app/Contents/MacOS/macsploit.dylib
+    mv ./libdiscord-rpc.dylib /Applications/Roblox.app/Contents/MacOS/libdiscord-rpc.dylib
     curl "https://raw.githubusercontent.com/devnorb/Polaris/main/ClientPatcher" -o "ClientPatcher"
     chmod +x ClientPatcher
     ./ClientPatcher "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes >/dev/null 2>&1
     mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
     echo -e "[...] Installed MacSploit"
+    exit
 }
 
 main
